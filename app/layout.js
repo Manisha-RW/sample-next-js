32fd7e5ed7211d3d6f96e65c23dc3818a3934a5e
Manisha <khattimanisha@rwit.io>
2024-05-23 12:39:47 +0530

    Created About us section

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import "./globals.css";
 
 export const metadata = {
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <About>{children}</About>
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        lg: ["48px"],
+      },
+      fontFamily: {
+        raleway: ["Raleway"],
+      },
+      fontWeight: {
+        semibold: "600",
+      },
+      colors: {
+        "default-blue": "#114B9F",
+      },
+      spacing: {
+        12: "10px",
+        22: "20px",
       },
     },
   },
