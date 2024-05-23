058288dc643d22a347d143aca85bc5368fad9a7b
Manisha <khattimanisha@rwit.io>
2024-05-23 17:01:33 +0530

    Created faq section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        raleway: "Raleway",
+        jakrata: "Plus Jakarta Sans",
+      },
+      colors: {
+        'default-blue': '#114B9F',
       },
     },
   },
