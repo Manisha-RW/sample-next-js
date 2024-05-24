a98fe5ea182c23b241ca080bba9fb66aa7f14589
Manisha <khattimanisha@rwit.io>
2024-05-24 12:13:15 +0530

    Added the basic structure files

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
+import Banner from '@/components/Banner'
+import MoreInfo from '@/components/MoreInfo'
+import OurBrand from '@/components/OurBrand'
+import OurClient from '@/components/OurClient'
 import  React from 'react'
 
 export default function Home() {
   return (
-    <div><h1>Page</h1></div>
+    <div>
+      <Banner/>
+      <MoreInfo/>
+      <OurBrand/>
+      <OurClient/>
+      <Banner/>
+      <Banner/>
+      </div>
   )
 }
---
+++ components/OurProjectss.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OurProjectss = () => {
+  return <div>OurProjectss</div>;
+};
+
+export default OurProjectss;
