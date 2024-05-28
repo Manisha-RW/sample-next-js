b229357e98889f117da4bfb1e1f3387dacad09ef
Manisha <khattimanisha@rwit.io>
2024-05-28 16:09:41 +0530

    Added responsiveness of about

--- components/About.jsx
+++ components/About.jsx
@@ -2,41 +2,36 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-center gap-20 height-[563px]">
-        <div className="sm:flex items-center w-[590px] h-[563px]">
-          <div className="relative  w-full h-full">
+    <div className="mt-20 lg:mt-40 overflow-hidden">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col lg:flex-row md:flex-col sm:flex-col justify-center gap-10 lg:gap-20 lg:items-center">
+          <div className="w-full lg:w-[590px] h-auto lg:h-[563px] relative">
             <img
-              src="/images/about-1.png"
-              className="rounded-lg w-[379px] h-[526px] absolute"
-            />
-            <img
-              src="/images/about-2.png"
-              className="rounded-lg absolute bottom-0 right-0 w-[318px] h-[415px] sm:top-[132px] sm:left-[255px]"
+              src="/images/about.png"
+              className="rounded-lg w-full h-auto lg:max-w-full"
+              alt="About us"
             />
           </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="text w-[572px] h-[370px]">
-            <h2 className="font-raleway font-semi-bold leading-10 text-5xl font-semibold leading-12 text-left text-lg mb-22">
+          <div className="text w-full lg:w-[572px]">
+            <h2 className="font-raleway font-medium text-3xl md:text-4xl lg:text-5xl leading-snug text-left mb-6">
               Who <span className="text-default-blue">We Are</span>
             </h2>
-            <div className="leading-6 font-railway">
-              <p className="mb-12">
-                Al Hayat Building Materials Company HBMC, established in
-                1983,is a member of Issam Kheiry Kabbani IKK Group of
-                Companies and is dedicated to supplying building materials of
-                different scopes. HBMC is known to be among the strongest in the
-                region for supplying bathroom finishing, kitchens and building
+            <div className="leading-6 font-raleway">
+              <p className="mb-6 md:mb-12 text-sm lg:text-base">
+                Al Hayat Building Materials Company HBMC, established in 1983,
+                is a member of Issam Kheiry Kabbani IKK Group of Companies and
+                is dedicated to supplying building materials of different
+                scopes. HBMC is known to be among the strongest in the region
+                for supplying bathroom finishing, kitchens and building
                 materials.
               </p>
-              <p>
+              <p className="text-sm lg:text-base">
                 Headquartered in Jeddah, HBMC’s 35 branches stretch out
                 geographically to cover the entire Kingdom of Saudi Arabia. 600
-                employees who aim to thrive in servicing our 4,000 + clients.In
+                employees who aim to thrive in servicing our 4,000+ clients. In
                 addition to offering a wide variety of water complementary
                 quality products, HBMC is a leader for introducing the latest
-                technology and innovations in the field. At HBMC, We offer an
+                technology and innovations in the field. At HBMC, we offer an
                 experience. Most of our products are directly imported from
                 manufacturers around the globe.
               </p>
