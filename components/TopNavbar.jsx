51e8c8c8448b2fe8f80dabd836f96c44219c6487
Manisha <khattimanisha@rwit.io>
2024-05-28 16:12:10 +0530

    Added responsiveness of top navbar

--- components/TopNavbar.jsx
+++ components/TopNavbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 const navData = [
   { label: "Personal", link: "/" },
-  { label: "Buisness", link: "/" },
+  { label: "Business", link: "/" },
   { label: "Wholesale", link: "/" },
   { label: "Sustainability", link: "/" },
   { label: "Investors", link: "/" },
@@ -11,24 +12,25 @@ const navData = [
 
 const TopNavbar = () => {
   return (
-    <div className="bg-default-blue text-white py-4 px-3">
+    <div className="bg-default-blue text-white py-4 sm:py-6 px-3">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
-        <div className="w-full md:w-1/2 md:text-center md:mb-0 mb-8"></div>
-        <div className="w-full md:w-1/2 md:text-center md:mb-0 mb-8">
-          <ul className="list-reset flex justify-end flex-wrap text-xs md:text-sm gap-x-10">
-            {navData?.map((i, index) => (
+      <div className="container mx-auto flex md:flex-row items-center justify-between">
+        <div className="w-full md:w-auto md:mb-0 mb-8 text-center">
+        </div>
+        <div className="w-full md:w-auto">
+          <ul className="list-reset flex justify-center md:justify-end flex-wrap text-xs md:text-sm gap-x-5 md:gap-x-10">
+            {navData?.map((item, index) => (
               <li className="font-open" key={index}>
-                <a href={i.link} className="text-white hover:text-white">
-                  {i.label}
-                </a>
+                <Link href={item.link} className="text-white hover:text-white">
+                  {item.label}
+                </Link>
               </li>
             ))}
           </ul>
