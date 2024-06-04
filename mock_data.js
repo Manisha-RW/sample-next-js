a05926635407b24fad7230a344850d887fd790ee
Manisha <khattimanisha@rwit.io>
2024-06-04 11:36:50 +0530

    Fixed conflicts

---
+++ mock_data.js
@@ -0,0 +1,257 @@
+export const aboutData = [
+  {
+    image: "/images/about.png",
+    title1: "Who",
+    title2: "We Are",
+    description1:
+      "Al Hayat Building Materials Company HBMC, established in 1983, is a member of Issam Kheiry Kabbani IKK Group of Companies and is dedicated to supplying building materials of different scopes. HBMC is known to be among the strongest in the region for supplying bathroom finishing, kitchens and building materials.",
+    description2:
+      "Headquartered in Jeddah, HBMC’s 35 branches stretch out geographically to cover the entire Kingdom of Saudi Arabia. 600 employees who aim to thrive in servicing our 4,000+ clients. In addition to offering a wide variety of water complementary quality products, HBMC is a leader for introducing the latest technology and innovations in the field. At HBMC, we offer an experience. Most of our products are directly imported from manufacturers around the globe.",
+  },
+];
+
+export const bannerData = {
+  bgImage: "/images/bgImg.jpg",
+  heading: "Shaping Bathrooms Into Something More",
+  subHeading:
+    "Transform Your Bathroom into a Luxurious Oasis - Innovative Design and Quality Products to Create Your Dream Bathroom",
+    btnData: { Btnlink: "/", BtnName: "Explore Products"},
+};
+
+export const contactData = {
+  image: "/images/contact.png",
+  title1: "Contact",
+  title2: "Us",
+  description:
+    "We’d love to speak to you and understand how we can work together to make graph analytics better.",
+};
+
+export const faqsData = {
+  title1: "FAQ",
+  title2: "s",
+  description:
+    "Suspendisse potenti. Vestibulum gravida turpis ut ipsum efficitur, idullamcorper sem semper",
+  faqData: [
+    {
+      heading: "Maecenas porttitor enim nec velit ultrices?",
+      description:
+        "Pellentesque iaculis, nisl et consectetur mattis, neque eros finibus elit, quis semper ipsum lorem vel augue. Aliquam erat volutpat. Nunc auctor sem a arcu ultrices molestie. Etiam magna risus, pulvinar ut metus at, consequat aliquet justo. Mauris egestas aliquam ligula ut molestie",
+    },
+    {
+      heading: "Maecenas porttitor enim nec velit ultrices?",
+      description:
+        "Pellentesque iaculis, nisl et consectetur mattis, neque eros finibus elit, quis semper ipsum lorem vel augue. Aliquam erat volutpat. Nunc auctor sem a arcu ultrices molestie. Etiam magna risus, pulvinar ut metus at, consequat aliquet justo. Mauris egestas aliquam ligula ut molestie",
+    },
+    {
+      heading: "Maecenas porttitor enim nec velit ultrices?",
+      description:
+        "Pellentesque iaculis, nisl et consectetur mattis, neque eros finibus elit, quis semper ipsum lorem vel augue. Aliquam erat volutpat. Nunc auctor sem a arcu ultrices molestie. Etiam magna risus, pulvinar ut metus at, consequat aliquet justo. Mauris egestas aliquam ligula ut molestie",
+    },
+    {
+      heading: "Maecenas porttitor enim nec velit ultrices?",
+      description:
+        "Pellentesque iaculis, nisl et consectetur mattis, neque eros finibus elit, quis semper ipsum lorem vel augue. Aliquam erat volutpat. Nunc auctor sem a arcu ultrices molestie. Etiam magna risus, pulvinar ut metus at, consequat aliquet justo. Mauris egestas aliquam ligula ut molestie",
+    },
+  ],
+};
+
+export const footerData = {
+  logoLink: "/",
+  logoImage: "/images/svgviewer-png-output.png",
+  title: "Proud member of IKK Group",
+  description: "Headquarters",
+  address: "Al Nakheel, Jeddah 23241, Saudi Arabia",
+  socialData: [
+    {
+      socialLink: "/",
+      image: "/images/facebook.png",
+    },
+    {
+      socialLink: "/",
+      image: "/images/instagram.png",
+    },
+    {
+      socialLink: "/",
+      image: "/images/twitter.png",
+    },
+    {
+      socialLink: "/",
+      image: "/images/youtube.png",
+    },
+  ],
+  linksData: [
+    {
+      heading: "Company",
+      links: [
+        { label: "Home", link: "/" },
+        { label: "Who we are", link: "/" },
+        { label: "Our Products", link: "/" },
+        { label: "Our Recent Projects", link: "/" },
+        { label: "Contact Us", link: "/" },
+      ],
+    },
+    {
+      heading: "Contact",
+      links: [
+        { label: "Home", link: "/" },
+        { label: "About", link: "/" },
+        { label: "Email", link: "/" },
+      ],
+    },
+    {
+      heading: "IKK Group",
+      links: [
+        { label: "Home", link: "/" },
+        { label: "Contact me", link: "/" },
+        { label: "About", link: "/" },
+      ],
+    },
+    {
+      heading: "Privacy Policy",
+      links: [
+        { label: "Home", link: "/" },
+        { label: "About", link: "/" },
+        { label: "Our Projects", link: "/" },
+      ],
+    },
+  ],
+  copyrightText: "© copyright OpulentOasis 2024",
+};
+
+export const navbarData = {
+  logoImg: "/images/svgviewer-png-output.png",
+  links: [
+    { link: "/", label: "Home" },
+    { link: "/", label: "About Us" },
+    { link: "/", label: "Our Brands" },
+    { link: "/", label: "Projects" },
+    { 
+      label: "Eng",
+      dropdown: [
+        { link: "/", label: "English" },
+        { link: "/", label: "Spanish" },
+        { link: "/", label: "French" },
+      ]
+    },
+  ],
+};
+
+export const ourClientData = {
+  title1: "What",
+  title2: "Our Clients",
+  title3: "Say",
+  clientData: [
+    {
+      image: "/images/client-1.jpg",
+      name: "Ralph Edwards",
+      position: "Senior SEO Executive",
+      description:
+        "I am absolutely delighted to share my positive experience as a client. From the moment I engaged with the company, I was met with exceptional professionalism and dedication to ensuring my satisfaction. The level of service provided was truly outstanding, and every interaction I had with the team was characterized by their unwavering commitment to excellence.",
+    },
+    {
+      image: "/images/client-1.jpg",
+      name: "Marina",
+      position: "CEO",
+      description:
+        "I am absolutely delighted to share my positive experience as a client. From the moment I engaged with the company, I was met with exceptional professionalism and dedication to ensuring my satisfaction. The level of service provided was truly outstanding, and every interaction I had with the team was characterized by their unwavering commitment to excellence.",
+    },
+    {
+      image: "/images/client-1.jpg",
+      name: "George",
+      position: "Manager",
+      description:
+        "I am absolutely delighted to share my positive experience as a client. From the moment I engaged with the company, I was met with exceptional professionalism and dedication to ensuring my satisfaction. The level of service provided was truly outstanding, and every interaction I had with the team was characterized by their unwavering commitment to excellence.",
+    },
+  ],
+};
+
+export const projectData = {
+  title1: "Our Recent",
+  title2: "Projects",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sollicitudin felis ut eros laoreet",
+  cardData: [
+    {
+      image: "/images/Project-1.jpg",
+      heading: "Al Raha Beach Buildings",
+      description: "Etiam nec nulla justo Curabitur tristique, ",
+      client: "Client: Al Dar",
+      location: "ABU DHABI",
+      iconImages: [
+        { title: "ABU DHABI", iconImg: "/images/icon-1.png" },
+        { title: "Al Futtaim Carillion", iconImg: "/images/icon-2.png" },
+        { title: "Turner", iconImg: "/images/icon-3.png" },
+      ],
+    },
+    {
+      image: "/images/Project-2.png",
+      heading: "Al Raha Beach Buildings",
+      description: "Etiam nec nulla justo Curabitur tristique, ",
+      client: "Client: Al Dar",
+      location: "ABU DHABI",
+      iconImages: [
+        { title: "ABU DHABI", iconImg: "/images/icon-1.png" },
+        { title: "Al Futtaim Carillion", iconImg: "/images/icon-2.png" },
+        { title: "Turner", iconImg: "/images/icon-3.png" },
+      ],
+    },
+    {
+      image: "/images/Project-3.png",
+      heading: "Al Raha Beach Buildings",
+      description: "Etiam nec nulla justo Curabitur tristique, ",
+      client: "Client: Al Dar",
+      location: "ABU DHABI",
+      iconImages: [
+        { title: "ABU DHABI", iconImg: "/images/icon-1.png" },
+        { title: "Al Futtaim Carillion", iconImg: "/images/icon-2.png" },
+        { title: "Turner", iconImg: "/images/icon-3.png" },
+      ],
+    },
+    {
+      image: "/images/Project-1.jpg",
+      heading: "Al Raha Beach Buildings",
+      description: "Etiam nec nulla justo Curabitur tristique, ",
+      client: "Client: Al Dar",
+      location: "ABU DHABI",
+      iconImages: [
+        { title: "ABU DHABI", iconImg: "/images/icon-1.png", },
+        { title: "Al Futtaim Carillion", iconImg: "/images/icon-2.png" },
+        { title: "Turner", iconImg: "/images/icon-3.png" },
+      ],
+    },
+  ],
+};
+
+export const statisticData = [
+  { label: "Years Of Experience", count: "+12" },
+  { label: "Success Projects", count: "+524" },
+  { label: "Active Projects", count: "+35" },
+  { label: "Happy Customers", count: "+420" },
+];
+
+export const navData = [
+  { label: "Personal", link: "/" },
+  { label: "Business", link: "/" },
+  { label: "Wholesale", link: "/" },
+  { label: "Sustainability", link: "/" },
+  { label: "Investors", link: "/" },
+];
+
+export const brandData = {
+  title1: "Our",
+  title2: "Brands",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis",
+  brandImages: [
+    { Img: "/images/Logo-1.png" },
+    { Img: "/images/Logo-2.png" },
+    { Img: "/images/Logo-3.png" },
+    { Img: "/images/Logo-4.png" },
+    { Img: "/images/Logo-5.png" },
+    { Img: "/images/Logo-6.png" },
+  ],
+};
+
+export const exploreBtnData = [
+  { Btnlink: "/", BtnName: "Explore Products"},
+];
