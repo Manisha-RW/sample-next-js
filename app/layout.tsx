e53e16094c3079b36d16d152605f501cfe94135d
Manisha <khattimanisha@rwit.io>
2024-06-06 13:34:26 +0530

    Aded global font on all components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,11 @@ import "./globals.css";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "../prismicio";
 import Layout from "../components/globals/Layout";
+import {Raleway} from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+})
 
 export const metadata = {
   title: "Next App",
@@ -13,12 +18,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
+      <main className={raleway.className}>
         <Layout>
           {children}
           <PrismicPreview repositoryName={repositoryName} />
           {/* <Footer props={footerData} /> */}
         </Layout>
+        </main>
       </body>
     </html>
   );
