b7baa3e53febd83c767e4bacb0729b7af3e9793c
Manisha <khattimanisha@rwit.io>
2024-06-07 13:29:33 +0530

    Executing

---
+++ components/globals/Layout.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+import { createClient } from '../../prismicio';
+import Footer from '../FooterSection/Footer';
+// import Navbar from '../Navbars/Navbar';
+interface LayoutProps {
+  children?: ReactNode;
+}
+async function Layout({ children }: LayoutProps) {
+  const client = createClient();
+  const navbar = await client.getSingle<any>('navbar');
+  const footer = await client.getSingle('footer');
+  return (
+    <div>
+      {/* <Navbar navbar={navbar}/> */}
+      {children}
+      <Footer footer={footer} />
+    </div>
+  );
+}
+export default Layout;
