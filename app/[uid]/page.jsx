74e5a0ed361c6971d3cde26e763f491fffa69e36
Manisha <khattimanisha@rwit.io>
2024-06-10 12:05:46 +0530

    Fixed the build issue

--- app/[uid]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@/prismicio";
-
-export default async function Page({ params }) {
-  const client = createClient();
-
-  const page = await client.getByUID("page", params.uid);
-
-  return <h1>{page.uid}</h1>;
-}
--- app/page.js
+++ app/page.js
@@ -1,47 +1,26 @@
-import React from "react";
-import { Raleway } from "next/font/google";
-import {
-  aboutData,
-  bannerData,
-  contactData,
-  faqsData,
-  navbarData,
-  ourClientData,
-  projectData,
-  statisticData,
-  navData,
-  brandData,
-} from "../mock_data";
+import { SliceZone } from "@prismicio/react";
 
-import Banner from "@/components/HeaderSection/Banner";
-import ContactUs from "@/components/Contact/ContactUs";
-import Faq from "@/components/FaqSection/Faq";
-import Navbar from "@/components/Navbars/Navbar";
-import OurBrand from "@/components/Brands/OurBrand";
-import OurClient from "@/components/OurClients/OurClient";
-import OurProjects from "@/components/ProjectSection/OurProjects";
-import Statistics from "@/components/MoreInfo/Statistics";
-import TopNavbar from "@/components/Navbars/TopNavbar";
-import About from "@/components/AboutUS/About";
+import { createClient } from "../prismicio";
+import { components } from "../slices";
 
-const raleway = Raleway({
-  subsets: ["latin"],
-  display: "swap",
-});
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
 
-export default function Home() {
   return (
-    <main className= {raleway.className}>
-      <TopNavbar props= { navData } />
-      <Navbar props= { navbarData } />
-      <Banner props= { bannerData } />
-      <Statistics props= { statisticData } />
-      <About props= { aboutData } />
-      <OurBrand props= { brandData } />
-      <OurClient props= { ourClientData } />
-      <OurProjects props={projectData}/>
-      <Faq props= { faqsData } />
-      <ContactUs props= { contactData } />
-    </main>
-  );
+    <>
+      <SliceZone slices={page.data.slices} components={components} />;
+    </>
+  )
+   
+}
+
+export async function generateMetadata() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
